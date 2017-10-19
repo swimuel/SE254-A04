16ee774c0385528bbe89cad657c9dc2ebcc653b7
swimuel <sambroadhead@icloud.com>
2017-10-19 18:19:22 +1300

    Starting q1

--- src/se254/a4/q1/Q1Main.java
+++ src/se254/a4/q1/Q1Main.java
@@ -1,5 +1,6 @@
 package se254.a4.q1;
-
+import java.lang.reflect.*;
+import java.util.Scanner;
 /**
  * SE254 Assignment 4 Question 1 Main. This class should be implemented to contain the functionality described in the
  * assignment handout. you may implement additional classes if you wish, but this class should be the main program entry
@@ -10,7 +11,28 @@ public class Q1Main {
 	public static void main(String[] args) {
 
 		// TODO Assignment 4, question 1.
-
+		try {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the name of a class: ");
+			String cName = sc.nextLine();
+			Class c = Class.forName(cName);
+			Object obj = c.newInstance();
+			System.out.println("Fields:");
+			for(Field f : c.getFields()) {
+				System.out.println(f.toString()+" "+f.get(obj).toString());
+			}
+			System.out.println("");
+			System.out.println("Methods:");
+			for(Method m: c.getMethods()) {
+				if(m.getParameterCount()==0) {
+					System.out.println(m);					
+				}
+			}
+			System.out.println("");
+			sc.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
